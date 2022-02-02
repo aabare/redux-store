f60ac4be447f56927c96f7c66f9d2ef4438ce237
Adam <a.6942@yahoo.com>
2022-02-02 12:18:29 -0500

    build

---
+++ client/build/precache-manifest.0e7c36476a185a52d36a294b941a8c4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f50cb1e88a45e7eab5def6e2d4d34a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58e8a126004c446636b3",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "eed7053ccba522c21720",
+    "url": "/static/js/2.dd67b8b7.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.dd67b8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e8a126004c446636b3",
+    "url": "/static/js/main.7880ce72.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
